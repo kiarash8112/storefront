9dcca3ba3bde7700f97d34b4459ff972cebaff58
kiarash <kiarash8112hajian@gmail.com>
2023-10-09 13:03:33 -0400

    add migrations

--- store/models.py
+++ store/models.py
@@ -10,8 +10,9 @@ class Collection(models.Model):
     featured_product = models.ForeignKey('Product',on_delete=models.SET_NULL,null=True,related_name="+")
 class Product(models.Model):
     title = models.CharField(max_length=225)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
