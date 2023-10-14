5d729c006f0bdefcf06e281aea8ab504a9b2fc2c
kiarash <kiarash8112hajian@gmail.com>
2023-10-14 15:18:46 -0400

    use serializer and return json in products urls

--- store/serializer.py
+++ store/serializer.py
@@ -4,3 +4,4 @@ class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=6,decimal_places=2)
+    
