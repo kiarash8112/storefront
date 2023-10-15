17bf85feba266fc7537a459ac7fa23c45fe996c5
kiarash <kiarash8112hajian@gmail.com>
2023-10-15 13:16:06 -0400

    add deseralizer for updating single product

--- store/views.py
+++ store/views.py
@@ -17,13 +17,19 @@ def product_list(request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save( )
-        return Response('ok')
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
 
-@api_view()
+@api_view(['GET','PUT'])
 def product_detail(request,id):
         product =  get_object_or_404(Product,pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+        if request.method == 'GET':
+            serializer = ProductSerializer(product)
+            return Response(serializer.data)
+        elif request.method =='PUT':
+            serializer = ProductSerializer(product,data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
 @api_view()
 def collection_detail(request,pk):
       return Response('ok')
