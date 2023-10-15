0f4247003cf65c74834c83fb17f0b2ca6b896397
kiarash <kiarash8112hajian@gmail.com>
2023-10-15 12:34:06 -0400

    replace serializer with  modelSerializer for avoiding dublication

--- store/serializer.py
+++ store/serializer.py
@@ -6,14 +6,17 @@ from .models import Product,Collection
 class CollectionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6,decimal_places=2)
-    product_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name = 'collection-detail',
-    )
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','unit_price','price_with_tax','collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # unit_price = serializers.DecimalField(max_digits=6,decimal_places=2)
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name = 'collection-detail',
+    # )
     def calculate_tax(self,product:Product):
         return product.unit_price * Decimal(1.1)
