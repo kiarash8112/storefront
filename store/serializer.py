8c2fb6f9012a0bc36296f0f58fb346589ae59dba
kiarash <kiarash8112hajian@gmail.com>
2023-10-15 12:23:46 -0400

    add serializing relationships for products and collections

--- store/serializer.py
+++ store/serializer.py
@@ -1,11 +1,19 @@
 from decimal import Decimal
 from rest_framework import serializers
-from .models import Product
+from .models import Product,Collection 
+
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=6,decimal_places=2)
     product_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-
+    collection = serializers.HyperlinkedRelatedField(
+        queryset = Collection.objects.all(),
+        view_name = 'collection-detail',
+    )
     def calculate_tax(self,product:Product):
         return product.unit_price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -10,7 +10,7 @@ from .serializer import ProductSerializer
 @api_view()
 def product_list(request):
     queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset,many = True)
+    serializer = ProductSerializer(queryset,many = True,context={'request':request})
     return Response(serializer.data)
 
 @api_view()
@@ -18,4 +18,6 @@ def product_detail(request,id):
         product =  get_object_or_404(Product,pk=id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    
+@api_view()
+def collection_detail(request,pk):
+      return Response('ok')
