2f7efbe90b45ddff0a928505a8d9f8d91ef7a8c9
kiarash <kiarash8112hajian@gmail.com>
2023-10-17 10:19:07 -0400

    add router in urls

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from . import views
 
+
+router = SimpleRouter()
+router.register("products", views.ProductViewSet)
+router.register("collection", views.CollectionViewSet)
+
 # URLConf
-urlpatterns = [
-    path('products/',views.ProductList.as_view()),
-    path('products/<int:pk>/',views.ProductDetails.as_view()),
-    path('collection/',views.CollectionList.as_view()),
-    path('collection/<int:pk>/',views.CollectionDetails.as_view()),
-]
+urlpatterns = router.urls
