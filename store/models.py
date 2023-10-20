3905e42c17c911dd1b93a98ad6ac9d45a2cb48ac
kiarash <kiarash8112hajian@gmail.com>
2023-10-20 01:49:48 -0400

    add paginatin and filter add cart items url(serializer,...)

--- store/models.py
+++ store/models.py
@@ -1,7 +1,6 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
-
-# Create your models here.
 
 
 class Promotion(models.Model):
@@ -12,7 +11,7 @@ class Promotion(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
-        'Product', on_delete=models.SET_NULL, null=True, related_name="+")
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
 
     def __str__(self) -> str:
         return self.title
@@ -22,14 +21,18 @@ class Collection(models.Model):
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=225)
+    title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(1)])
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
     last_update = models.DateTimeField(auto_now=True)
-    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion)
+    collection = models.ForeignKey(
+        Collection, on_delete=models.PROTECT, related_name='products')
+    promotions = models.ManyToManyField(Promotion, blank=True)
 
     def __str__(self) -> str:
         return self.title
@@ -39,27 +42,28 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
-    MEMBERSHIP_BROONZE = 'B'
+    MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
+
     MEMBERSHIP_CHOICES = [
-        ('B', 'BRONZE'),
-        ('S', 'SILVER'),
-        ('G', 'GOLD'),
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
     ]
-    first_name = models.CharField(max_length=225)
-    last_name = models.CharField(max_length=225)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.TextField(max_length=225)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
     membership = models.CharField(
-        null=True, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BROONZE, max_length=1)
-    birth_date = models.DateField(null=True)
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
     class Meta:
-        db_table = 'store_customers'
-        indexes = [
-            models.Index(fields=['last_name', 'first_name'])
-        ]
+        ordering = ['first_name', 'last_name']
 
 
 class Order(models.Model):
@@ -69,8 +73,9 @@ class Order(models.Model):
     PAYMENT_STATUS_CHOICES = [
         (PAYMENT_STATUS_PENDING, 'Pending'),
         (PAYMENT_STATUS_COMPLETE, 'Complete'),
-        (PAYMENT_STATUS_FAILED, 'Faild')
+        (PAYMENT_STATUS_FAILED, 'Failed')
     ]
+
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
@@ -79,7 +84,8 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    product = models.ForeignKey(
+        Product, on_delete=models.PROTECT, related_name='orderitems')
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
@@ -87,7 +93,8 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE)
 
 
 class Cart(models.Model):
@@ -99,14 +106,17 @@ class CartItem(models.Model):
     cart = models.ForeignKey(
         Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)]
+    )
 
     class Meta:
         unique_together = [['cart', 'product']]
 
 
 class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='reviews')
     name = models.CharField(max_length=255)
     description = models.TextField()
     date = models.DateField(auto_now_add=True)
--- store/urls.py
+++ store/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path
+from django.urls.conf import include
 from rest_framework_nested import routers
 from . import views
 
-
 router = routers.DefaultRouter()
-router.register(prefix="products", viewset=views.ProductViewSet)
-router.register(prefix="collections", viewset=views.CollectionViewSet)
-router.register(prefix="carts", viewset=views.CartViewSet)
+router.register('products', views.ProductViewSet, basename='products')
+router.register('collections', views.CollectionViewSet)
+router.register('carts', views.CartViewSet)
 
 products_router = routers.NestedDefaultRouter(
     router, 'products', lookup='product')
-
 products_router.register('reviews', views.ReviewViewSet,
                          basename='product-reviews')
+
+carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_router.register('items', views.CartItemViewSet, basename='cart-items')
+
 # URLConf
-urlpatterns = router.urls + products_router.urls
+urlpatterns = router.urls + products_router.urls + carts_router.urls
