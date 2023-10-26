9f504aab830e556e32c8c88ba6ae4de2f73b88ed
kiarash <kiarash8112hajian@gmail.com>
2023-10-26 05:14:23 -0400

    add unit tests for collection endpoint

---
+++ store/tests/conftest.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+import pytest
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+    return do_create_collection
+
+
+@pytest.fixture
+def authenticate(api_client):
+    def do_authenticate(is_staff=False):
+        return api_client.force_authenticate(user=User(is_staff=is_staff))
+    return do_authenticate
