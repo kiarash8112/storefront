2cc7fe1e7625d85a655d9f66dc5276f5c48b2d95
kiarash <kiarash8112hajian@gmail.com>
2023-10-26 15:19:13 -0400

    add cache configuration

--- storefront/settings.py
+++ storefront/settings.py
@@ -167,3 +167,13 @@ SIMPLE_JWT = {
 }
 
 CELERY_BROKER_URL = 'redis://localhost:6379/1'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
