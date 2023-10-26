56e2fa84a471dde4c06f16fe0ca6e6069a5c3d09
kiarash <kiarash8112hajian@gmail.com>
2023-10-26 15:46:54 -0400

    imp cache use decorator

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,11 @@
 from django.core.cache import cache
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 import requests
 
 
+@cache_page(5*60)
 def say_hello(request):
-    key = 'httpbin_result'
-    if cache.get(key) is None:
-        response = requests.get('https://httpbin.org/delay/2')
-        data = response.json()
-        cache.set(key, data)
-    return render(request, 'hello.html', {'name': cache.get(key)})
+    response = requests.get('https://httpbin.org/delay/2')
+    data = response.json()
+    return render(request, 'hello.html', {'name': data})
